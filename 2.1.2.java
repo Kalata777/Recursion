0f5a5cc9a359c31cb9d358fe23da80028559d342
Kalata777 <72138487+Kalata777@users.noreply.github.com>
2021-02-12 07:59:32 +0200

    Create 2.1.2.java

---
+++ 2.1.2.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+	   	 System.out.println(Even(3));
+	}
+
+	public static boolean Even(int n) {
+		
+	n-=2;
+	
+if(n<=0) {
+if(n==0) {
+	return true;
+}
+	return false;
+	 }
+	}else {
+		return Even(n);
+	
+	}
